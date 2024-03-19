9f2a5c9e464386f6f737f034999d5e45290e2ceb
dewhursl <dewhursl@tcd.ie>
2024-03-19 00:13:28 +0000

    Implemented basic speech animation

--- frontend-react-app/src/components/AiAvatar.jsx
+++ frontend-react-app/src/components/AiAvatar.jsx
@@ -1,12 +1,53 @@
-import react from "react";
+import React, { useEffect, useState } from "react";
 import Avatar from "avataaars";
 import "../styles/AiAvatar.css";
 
-export default function AiAvatar() {
+// Delay (in ms) between each animation frame
+const frameInterval = 1000 / 5;
+
+export default function AiAvatar({ messages }) {
+  // TODO Add more transition frames for smooth speach animation
+  const speakFrames = ["Smile", "Default"];
+
+  // Current number of animation frames remaining
+  const [speakDuration, setSpeakDuration] = useState(0);
+
+  // Speech animation effect
+  useEffect(() => {
+    if (messages.length > 0) {
+      const lastMessage = messages[messages.length - 1];
+      if (lastMessage.sender == "System") {
+        // Determine number of speach frames.
+        // TODO Replace placeholder calculation with calculated value based on duration of text to speach
+        setSpeakDuration(Math.trunc(lastMessage.text.length * 0.1));
+
+        // Sequence animation frames
+        const intervalId = setInterval(() => {
+          setSpeakDuration((prevSpeakDuration) => {
+            if (prevSpeakDuration > 0) {
+              return prevSpeakDuration - 1;
+            } else {
+              clearInterval(intervalId);
+              return 0;
+            }
+          });
+        }, frameInterval);
+      } else {
+        // Cancel current speech animation when user submits a message
+        setSpeakDuration(0);
+      }
+    }
+  }, [messages]);
+
   return (
     <div className="container-avatar">
-      <Avatar 
-        style={{width: '100%', height: '100%'}}
+      <Avatar
+        style={{ width: "100%", height: "100%" }}
+        mouthType={
+          speakDuration > 0
+            ? speakFrames[speakDuration % speakFrames.length]
+            : "Default"
+        }
       />
     </div>
   );
